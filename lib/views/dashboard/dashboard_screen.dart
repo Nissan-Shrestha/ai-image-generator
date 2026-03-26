24cca42d0f9157854deb31ebe0f6645da106e708
Nissan Shrestha <nissan.shrestha.a23@icp.edu.np>
2026-03-26 14:01:52 +0545

    dashboard screen ui

---
+++ lib/views/dashboard/dashboard_screen.dart
@@ -0,0 +1,80 @@
+import 'package:ai_image_generator_app/core/constants/colors.dart';
+import 'package:ai_image_generator_app/core/constants/textstyle.dart';
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text("Welcome :)", style: headingStyle),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text("Choose an action", style: headingStyle),
+              SizedBox(height: 10),
+              Row(
+                spacing: 10,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                    ),
+                    onPressed: () {},
+                    child: Text("Generate an image", style: buttonTextStyle),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: secondaryColor,
+                      ),
+                      onPressed: () {},
+                      child: Text("Edit an image", style: buttonTextStyle),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text("Recent Images", style: headingStyle),
+              SizedBox(height: 10),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?semt=ais_hybrid&w=740&q=80",
+                        ),
+                      ),
+                      color: primaryColor,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/onboarding/onboarding_screen.dart
+++ lib/views/onboarding/onboarding_screen.dart
@@ -1,3 +1,6 @@
+import 'package:ai_image_generator_app/core/constants/colors.dart';
+import 'package:ai_image_generator_app/core/constants/textstyle.dart';
+import 'package:ai_image_generator_app/views/dashboard/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -6,9 +9,34 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Onboarding"),
-        
+      backgroundColor: backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Welcome to AI Image Generator", style: headingStyle),
+              SizedBox(height: 5),
+              Text("Generate or edit your first image", style: bodyStyle),
+              SizedBox(height: 5),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: primaryColor),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (_) => DashboardScreen()),
+                  );
+                },
+
+                child: Text("Lets Go", style: buttonTextStyle),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
