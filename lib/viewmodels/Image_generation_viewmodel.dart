8ecda2a9c5ff85683dd08d2bc4d90dc33ed369b2
Nissan Shrestha <nissan.shrestha.a23@icp.edu.np>
2026-03-29 14:36:57 +0545

    mock image generation finishied

--- lib/viewmodels/Image_generation_viewmodel.dart
+++ lib/viewmodels/Image_generation_viewmodel.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 class ImageGenerationViewmodel extends ChangeNotifier {
   bool isLoading = false;
 
+  String? generatedImageUrl;
+
   Future<void> generateImage(String prompt) async {
     if (isLoading) return;
     if (prompt.trim().isEmpty) return;
 
+    generatedImageUrl = null;
     isLoading = true;
     notifyListeners();
 
@@ -14,6 +17,8 @@ class ImageGenerationViewmodel extends ChangeNotifier {
 
     await Future.delayed(const Duration(seconds: 2)); // simulate API
 
+    generatedImageUrl =
+        "https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg";
     isLoading = false;
     notifyListeners();
   }
--- lib/views/generation/image_generation_screen.dart
+++ lib/views/generation/image_generation_screen.dart
@@ -1,6 +1,5 @@
 import 'package:ai_image_generator_app/core/constants/colors.dart';
 import 'package:ai_image_generator_app/core/constants/textstyle.dart';
-import 'package:ai_image_generator_app/data/models/prompt.dart';
 import 'package:ai_image_generator_app/viewmodels/Image_generation_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,90 +22,109 @@ class _ImageGenerationScreenState extends State<ImageGenerationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ImageGenerationViewmodel(),
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: backgroundColor,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Generate Image", style: subheadingStyle),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Consumer<ImageGenerationViewmodel>(
-                builder: (context, vm, _) {
-                  return TextFormField(
-                    enabled: !vm.isLoading,
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: "Enter ur prompt",
-                      suffixIcon: vm.isLoading
-                          ? Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: CircularProgressIndicator(strokeWidth: 3),
-                            )
-                          : IconButton(
-                              onPressed: () {
-                                final prompt = _controller.text;
-                                vm.generateImage(prompt);
-                                _controller.clear();
-                              },
-                              icon: Icon(Icons.arrow_forward),
-                            ),
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Generate Image", style: subheadingStyle),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Consumer<ImageGenerationViewmodel>(
+              builder: (context, vm, _) {
+                return TextFormField(
+                  enabled: !vm.isLoading,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: "Enter ur prompt",
+                    suffixIcon: vm.isLoading
+                        ? Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CircularProgressIndicator(strokeWidth: 3),
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              final prompt = _controller.text;
+                              vm.generateImage(prompt);
+                              _controller.clear();
+                            },
+                            icon: Icon(Icons.arrow_forward),
+                          ),
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.transparent),
                     ),
-                  );
-                },
-              ),
-              SizedBox(height: 20),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 20),
 
-              //i was just messing around
-              // Text("Previous Prompts", style: bodyStyle),
-              // Consumer<ImageGenerationViewmodel>(
-              //   builder: (context, vm, _) {
-              //     if (vm.history.isEmpty) {
-              //       return Text("No history");
-              //     } else {
-              //       return ListView.builder(
-              //         shrinkWrap: true,
-              //         itemCount: vm.history.length,
-              //         itemBuilder: (BuildContext context, int index) {
-              //           final prompt = vm.history[index];
-              //           return ListTile(
-              //             leading: Icon(Icons.history),
-              //             title: Text(prompt),
-              //           );
-              //         },
-              //       );
-              //     }
-              //   },
-              // ),
-              Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                alignment: Alignment.center,
-                child: Text(
-                  "Your generated image will appear here",
-                  style: bodyStyle,
-                ),
-              ),
-            ],
-          ),
+            //i was just messing around
+            // Text("Previous Prompts", style: bodyStyle),
+            // Consumer<ImageGenerationViewmodel>(
+            //   builder: (context, vm, _) {
+            //     if (vm.history.isEmpty) {
+            //       return Text("No history");
+            //     } else {
+            //       return ListView.builder(
+            //         shrinkWrap: true,
+            //         itemCount: vm.history.length,
+            //         itemBuilder: (BuildContext context, int index) {
+            //           final prompt = vm.history[index];
+            //           return ListTile(
+            //             leading: Icon(Icons.history),
+            //             title: Text(prompt),
+            //           );
+            //         },
+            //       );
+            //     }
+            //   },
+            // ),
+            Consumer<ImageGenerationViewmodel>(
+              builder: (context, vm, _) {
+                return Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  child: () {
+                    if (vm.isLoading) {
+                      return Center(
+                        child: CircularProgressIndicator(strokeWidth: 3),
+                      );
+                    }
+                    if (vm.generatedImageUrl == null) {
+                      return Text(
+                        "Your generated image will appear here",
+                        style: bodyStyle,
+                        textAlign: TextAlign.center,
+                      );
+                    }
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.network(
+                        vm.generatedImageUrl!,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                    );
+                  }(),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
